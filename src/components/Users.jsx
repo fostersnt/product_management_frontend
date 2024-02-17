96357f11c19b683ec22f52ec292c87cc2dd97243
= <=>
2024-02-17 17:42:17 +0000

    Added users component

---
+++ src/components/Users.jsx
@@ -0,0 +1,39 @@
+import DataTable from 'react-data-table-component';
+
+
+const columns = [
+	{
+		name: 'Title',
+		selector: row => row.title,
+		sortable: true,
+	},
+	{
+		name: 'Year',
+		selector: row => row.year,
+		sortable: true,
+	},
+];
+
+const data = [
+  	{
+		id: 1,
+		title: 'Beetlejuice',
+		year: '1988',
+	},
+	{
+		id: 2,
+		title: 'Ghostbusters',
+		year: '1984',
+	},
+]
+
+export default function Users()
+{
+    return(<div>
+        <h1>Hello world</h1>
+        <DataTable
+			columns={columns}
+			data={data}
+		/>
+    </div>)
+}
