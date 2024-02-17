35001a7bbb4104757f3a22d3f53c8e67d17bd973
= <=>
2024-02-17 18:10:11 +0000

    Added api call

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,39 +1,67 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import DataTable from 'react-data-table-component';
+// import { axios } from './../../node_modules/axios/dist/esm/axios';
 
+export default function Users() {
+    const [users, setUsers] = useState([]);
 
-const columns = [
-	{
-		name: 'Title',
-		selector: row => row.title,
-		sortable: true,
-	},
-	{
-		name: 'Year',
-		selector: row => row.year,
-		sortable: true,
-	},
-];
+    useEffect(() => {
+        const output = async () => {
+            // const axios = require('axios');
 
-const data = [
-  	{
-		id: 1,
-		title: 'Beetlejuice',
-		year: '1988',
-	},
-	{
-		id: 2,
-		title: 'Ghostbusters',
-		year: '1984',
-	},
-]
+            const options = {
+                method: 'GET',
+                url: 'https://odds.p.rapidapi.com/v4/sports',
+                params: { all: 'true' },
+                headers: {
+                    'X-RapidAPI-Key': '44f54adecamsh757b964b6f94522p112bbdjsn81d9f06f647d',
+                    'X-RapidAPI-Host': 'odds.p.rapidapi.com'
+                }
+            };
 
-export default function Users()
-{
-    return(<div>
+            try {
+                const response = await axios.request(options);
+                setUsers(response.data);
+                console.log(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        output()
+    });
+
+    const columns = [
+        {
+            name: 'Title',
+            selector: row => row.title,
+            sortable: true,
+        },
+        {
+            name: 'key',
+            selector: row => row.key,
+            sortable: true,
+        },
+        {
+            name: 'group',
+            selector: row => row.group,
+            sortable: true,
+        },
+        {
+            name: 'description',
+            selector: row => row.description,
+            sortable: true,
+        },
+    ];
+
+    const data = users;
+
+    return (<div>
         <h1>Hello world</h1>
         <DataTable
-			columns={columns}
-			data={data}
-		/>
+            columns={columns}
+            data={data}
+            pagination
+        />
     </div>)
 }
